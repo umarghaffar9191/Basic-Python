d3e58ff8e3e45401e19309a89985efd0856dc75c
umarghaffar9191 <umarghaffar9191@gmail.com>
2024-03-19 00:19:23 +0500

    Some changes in Str

--- str_base.py
+++ str_base.py
@@ -9,26 +9,67 @@ usman = 'Malik'
 
 # _________________________________________Slicing_______________________
 
-hello = 'Programmer'
+# hello = 'Programmer'
 
-# Get the index number
-print(hello[6])
+# # Get the index number
+# print(hello[6])
 
-# Slice from the start
-print(hello[:4])
+# # Slice from the start
+# print(hello[:4])
 
-# slice to the end
-print(hello[3:])
+# # slice to the end
+# print(hello[3:])
 
-# get the last index number 
-print(hello[-1])
+# # get the last index number 
+# print(hello[-1])
 
-# get the second last index number 
-print(hello[-2])
+# # get the second last index number 
+# print(hello[-2])
 
-# reverse string
-print(hello[::-1])
+# # reverse string
+# print(hello[::-1])
 
 # __________________________________________Modify_string______________________
 
-# i am going to start my new data
+# Upper(),                         it makes all characters of string in upper case
+a = 'MyWorld'
+# print(a.upper())
+
+#lower()
+
+
+#title
+
+#replace
+
+
+#split
+
+# ________________________________________________Join Strings_______________________
+
+
+
+
+
+
+#____________________________________________________String fromat_______________________
+
+# student_name = input('What is your name: ')
+# student_age = input('What is your age: ')
+# student_gender = input('What is your gender: ')
+
+# data = f'Name: {student_name}, Age: {student_age}, Gender: {student_gender}'
+# print(data)
+
+
+# first_name = input('First_name: ')
+# last_name = input('Last_name: ')
+
+# print(f'First Name: {first_name}, Last Name: {last_name}')
+
+# name = 'Nida'
+# Age = '24'
+# Designation = 'Housewife'
+# Gender = 'Female'
+
+# print(f'{name},{Age},{Designation},{Gender}')
