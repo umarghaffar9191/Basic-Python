76eb6a5a8bf9a3cfe125506182fd0dd1d4f0337b
umarghaffar9191 <umarghaffar9191@gmail.com>
2024-03-18 23:27:08 +0500

    String_data

---
+++ str_base.py
@@ -0,0 +1,33 @@
+
+# _______________________________________syntax______________________________
+
+"helo", 'helo',
+
+# Assign string to a variable
+umar = 'helloworld'
+usman = 'Malik'
+
+# _________________________________________Slicing_______________________
+
+hello = 'Programmer'
+
+# Get the index number
+print(hello[6])
+
+# Slice from the start
+print(hello[:4])
+
+# slice to the end
+print(hello[3:])
+
+# get the last index number 
+print(hello[-1])
+
+# get the second last index number 
+print(hello[-2])
+
+# reverse string
+print(hello[::-1])
+
+# __________________________________________Modify_string______________________
+
