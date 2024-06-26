d2cbd16bb1e0c4265f30a7cf315f9778037fc587
Muhammad Umar <163880436+umarghaffar9191@users.noreply.github.com>
2024-06-26 09:44:36 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,112 @@
+# BISMILLAH IRRAHMAN IRRAHEEM
+#-----------------------OPERATORS-----------------------\
+    # a operator is a symbol that performs  the operation
+    #      following are the operators 
+    #   1: Arithmetic operators 
+    # 2: relational operators / comparison operators
+    # 3: logical operators
+    # 4: assignment operators
+    # 5: bitwise operators 
+    # 6: membership operators
+    # 7: identity operators   
+    
+    #---------------------------ARITHMETIC OPERATORS --------------------
+
+# arithmetic operators are used to perforn a basic arithmetic operatioons like addition 
+# , subtraction , division etc
+
+
+#     operators   |   meaning      |    example  |   result 
+#      +          |   addition     |     4+2     |     6  
+#      -          |  subtraction   |     4-2     |     2
+#      *          |  multiplication|     4*2     |     8
+#      /          | division       |    4/2      |     2 
+#      %          |modulus         |    5%2      |     1      
+#                 |operator to get |             |
+#                 | reminder in the|             |
+#                 |integer division|             |
+# 
+#     **          | exponent       |   5**2      |    25    
+#                 |                | 5 ki power 2|  
+
+#     //          |   integer      |   5//2      |   2   (5/2 = 2.5 ye round figure krke 2 answer dega)
+#                 |division / floor|   -5//2     |   -3  (-5/2 = -2.5 ye roung figure krk -3 answer dega)
+#                 | division       |             |
+
+
+#    ----------------------------assignment operators ------------------------------------
+# assignment operators are used to perform arithmetic operation while assigning a value to a variable
+# example 
+# a = 10    ,    b=  20     , m - 15 
+#  OPERATOR  |   EXAMPLE     |   EQUIVALENT EXPRESSION( M= 15)    |   RESULT 
+#     =      | y = a+ b      |  y = 10 + 20                       |   30 
+#    +=      | m+=10         |  m = m+10                          |  25
+#    -=      | m-=10         |  m = m-10                          |   5
+#    *=      | m*= 10        | m = m*10                           | 150 
+#    /=      | m/=10         | m = m/10                           | 1
+#    %=      | m%=10         | m = m%10                           | 5
+#    **=     | m**=10        | m= m **10  or m = m ki power 10    | 225
+#    //=     | m//=10        | m = m//10                          | 1
+
+#----------------------RELATIONAL OPERATORS----------------
+#    relational operators are used to compare the value of the operands ( expressions )
+#   to produce a logical value . a logical value is either True or False 
+
+#    OPERATORS |        MEANING          | EXAMPLE  |    RESULT   
+#      <       | less than               |  5<2     |  False 
+#      >       | greather than           |  5>2     |   True 
+#      <=      | less than or  equal to  | 5<=2     |  False 
+#      >=      | greather than or equalto|  5>=2    | True 
+#      ==      |  equal to               |  5==2    |    False 
+#     !=       | not equal to            | 5!=2     |    True
+
+#--------------------LOGICAL OPERATORS -----------------------------------
+# LOgical operators are used to connect more relational operators to form a complex expressions 
+# called logical expression a value obtained by the evaluating a logical expression is always logical i.e
+# either True or False
+
+
+#    operators |      MEANING    |     EXAMPLE    |      RESULT 
+#        and   |  logical and    |(5<2) and (5>2) |    False 
+#     or       | logica l or     | (5<2) or (5>2) |    True
+#     not      |   logical not   | not (5<2)      |     True
+
+
+#      -------------and----------
+#   operation 1   |      operation 2  |    Result
+#   True          |  True            |     True 
+#   True          | false            |   false 
+#  False          | false            | False 
+#False            | True             |  False
+#   True          |  expression      | expression 
+#  False          | expression       | False 
+
+# example of True and expression , False and expression 
+
+# True and expression = expression 
+# True and expression 1 and expression 2 = expression 2 
+# False and expression 1 = False 
+# False and expression 1 and expression 2 =False 
+
+
+#----------------------or ----------------------
+
+# operation 1    | operation 2    |  result 
+#  True          | True           |  True 
+# True           | False          |  True
+# False          | True           |  True
+# False          | false          | False 
+# True           | expression     |  True 
+#  False         |expression      | expression
+
+
+# True or expressoion 1 or expression 2 = True 
+#  False or  expression 1 or expression 2 = expression 1 
+
+
+#-----------------not ----------------------
+# operand        |      result 
+# False          |   True 
+#  True          |    False
+
+
