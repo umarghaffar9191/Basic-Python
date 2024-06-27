1182397252fcd21c91ac89c2475a845862e917b6
Muhammad Umar <163880436+umarghaffar9191@users.noreply.github.com>
2024-06-27 21:24:58 +0500

    Add files via upload

---
+++ escape.sequence.py
@@ -0,0 +1,25 @@
+#-------------------------------------escape sequence------------------------
+# escape sequence - escape sequence are control character used to move the cursor and print characters such as ',".\ and so n
+
+
+
+
+#  escape sequence                  meaning 
+#   \a                                  bell
+#   \b                                  backspace
+#   \f                                  formfeed
+#   \n                                  new line
+#   \r                                  carriage return
+#   \t                                  horizontal tab
+#   \v                                  vertical tab
+#   \newline                            backslash and newline ignored 
+
+#     \\                               backslash
+#     \'                              single quote
+#   \"                                double quote
+print("welcome home")
+print("welcome \n home")
+print("welcome\t home")
+print("welcome\"home\"")
+print("welcome 'home' ")
+print('welcoome\'home\'')
